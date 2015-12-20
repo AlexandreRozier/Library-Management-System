8aa538b195fbf0199fc2eef6997341f948ce8b16
hxkm <alexandre.rozier@student.ecp.fr>
2015-12-20 23:00:34 +0100

    uml

--- src/lms_rozier_convers/core/AbstractFactory.java
+++ src/lms_rozier_convers/core/AbstractFactory.java
@@ -1,9 +1,11 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package lms_rozier_convers.core;
 
-/**
- * 16/11/2015.
- * The Abstract class of the factories : used in the Factory Pattern
- */
 public abstract class AbstractFactory {
-
+    public AbstractFactory() {
+    }
 }
