bf9b4b1fb3897e01b6d5bddb43adb11bb925fe99
Kairs0 <arnaud.convers@student.ecp.fr>
2015-12-21 12:24:11 +0100

    Parsing + squelette du switch

--- src/lms_rozier_convers/CLIU/UserInterface.java
+++ src/lms_rozier_convers/CLIU/UserInterface.java
@@ -4,6 +4,7 @@ import lms_rozier_convers.core.library.Library;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by hx on 20/12/2015.
@@ -15,7 +16,7 @@ public class UserInterface {
 
 
     /**
-     * CONSTRUCTO
+     * CONSTRUCTOR
      * A constructor used to create an interface not linked to a library, for instance when
      * one wants to create a library via the CLUI.
      */
@@ -35,6 +36,67 @@ public class UserInterface {
     }
 
     private void launch() {
+        boolean open = true;
+        while(open){
+            Scanner sc = new Scanner(System.in);
+            String str = sc.nextLine();
+            //Parsing : we assume that the user will separate command and variables with a space (like in a Linux Console)
+            String[] commandAndParameters ;
+            commandAndParameters = str.split(" ",5);
+
+            switch (commandAndParameters[0]){
+                case("create_library"):
+
+                    break;
+                case("add_room"):
+
+                    break;
+                case("add_bookcase"):
+
+                    break;
+                case("add_item"):
+                    break;
+                case("store_items"):
+
+                    break;
+                case("unstore_items"):
+
+                    break;
+                case("list_items"):
+
+                    break;
+                case("list_room"):
+
+                    break;
+                case("list_bookcase"):
+
+                    break;
+                case("find_items"):
+
+                    break;
+                case("search_title"):
+
+                    break;
+                case("add_member"):
+
+                    break;
+                case("borrow_item"):
+
+                    break;
+                case("check_borrowed"):
+
+                    break;
+                case("exit"):
+                    open=false;
+                    break;
+                default:
+                    System.out.println("Invalid command. Please be attentive to use low case.");
+
+
+
+
+            }
+        }
         // TODO ARNAUD : implémenter ici le parsing, le switch, etc :3
         //KEUR
     }
